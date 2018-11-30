14a63b9ea68e7ed24fd6772e20b18cec7948ee5e
brenden <brenden@brendens-air.dhcp.nd.edu>
2018-11-29 23:48:06 -0500

    Added Server and Handler Methods

---
+++ cacheOverflow2/src/cacheOverflow2/Server.java
@@ -0,0 +1,40 @@
+package cacheOverflow2;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javafx.application.Platform;
+
+public class Server {
+	private ServerSocket serverSocket;
+	private int clientCount;
+	
+	public Server() {
+		clientCount = 0;
+	}
+	
+	
+	
+	public void initialize() {
+		new Thread( () -> {
+			try {
+				serverSocket = new ServerSocket(8000);
+				
+				while(true) {
+					Socket socket = serverSocket.accept();
+					clientCount++;
+					
+					Platform.runLater( () -> {
+						System.out.println(clientCount);
+					});
+					
+					new Thread(new Handler(socket)).start();
+				}
+			}
+			catch(IOException ex){
+				System.err.println(ex);
+			}
+		}).start();
+	}
+}
