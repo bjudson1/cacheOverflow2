506b651b76895704cec51076163718e05b2704ad
Harry Gebremedhin <hgebreme@nd.edu>
2018-11-30 02:55:04 -0500

    Some ui functionality ... loading works

---
+++ cacheOverflow2/src/cacheOverflow2/RegistrationFormApplication.java
@@ -0,0 +1,27 @@
+package cacheOverflow2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RegistrationFormApplication extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("registration_form.fxml"));
+		Parent root = loader.load();
+		
+//		Parent root = FXMLLoader.load(getClass().getResource("/registration_form.fxml"));
+		
+		primaryStage.setTitle("Login Form");
+		primaryStage.setScene(new Scene(root, 800, 500));
+		primaryStage.show();
+	}
+
+	public static void main(String[] args){
+		launch(args);
+	}
+}
