785b24e6c89ca14cb9f03b28ef56127fdeb2221b
brenden <brenden@brendens-air.dhcp.nd.edu>
2018-12-03 20:22:50 -0500

    many changes

--- cacheOverflow2/src/cacheOverflow2/Main.java
+++ cacheOverflow2/src/cacheOverflow2/Main.java
@@ -5,11 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//UI ui = new UI();
-		UI.run(args);
+		UI ui = new UI();
+		ui.run(args);
 		
-		Server server = new Server();
-		server.initialize();
+		//Server server = new Server();
+		//server.initialize();
 	}
 
 }
